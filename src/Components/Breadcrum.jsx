ff96581fbd637ee7d86627e90b1baf0711b6d7b5
Banda-Manjunatha <manjunathmaahi1255@gmail.com>
2024-03-16 14:05:06 +0530

    initial commit

--- src/Components/Breadcrum.jsx
+++ src/Components/Breadcrum.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../styles/Breadcrum.css'
+import { Link } from 'react-router-dom';
 
 const Breadcrum = (props) => {
     const {product} = props;
   return (
     <div className='breadcrum'>
        <div className='container'>
-       Home <img src="https://res.cloudinary.com/dmn7qksnf/image/upload/v1709533333/breadcrum_arrow_lam5pa.png" alt="" /> Product <img src="https://res.cloudinary.com/dmn7qksnf/image/upload/v1709533333/breadcrum_arrow_lam5pa.png" alt="" /> {product.name}
+       <Link to='/home' className='link-bread'>Home</Link> <img src="https://res.cloudinary.com/dmn7qksnf/image/upload/v1709533333/breadcrum_arrow_lam5pa.png" alt="" /> {product.name}
        </div>
     </div>
   )
